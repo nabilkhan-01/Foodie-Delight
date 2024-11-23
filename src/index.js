0408edece48d100f703bd17f37f92d59585a9231
Nabil Khan <89152492+Dynamickhan@users.noreply.github.com>
2024-11-23 22:45:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Add BrowserRouter with the basename */}
+    <BrowserRouter basename="/Foodie-Delight">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
